36ce3a53d9282dcb9536113ef525a40928aadded
Ariel Eps <butafliko@gmail.com>
2023-05-20 13:33:38 +0300

    adding registration forms for Patient and Doctor

--- PneumoniaApp/Authentication/routes.py
+++ PneumoniaApp/Authentication/routes.py
@@ -4,10 +4,18 @@ Authentication = Blueprint('Authentication', __name__)
 
 
 @Authentication.route("/")
-def AuthScreen():
+def Login():
     return render_template('Login.html')
 
 
 @Authentication.route("/ChooseAuth")
 def ChooseAuth():
     return render_template('ChooseAuth.html')
+
+@Authentication.route("/DoctorRegister",  methods=['POST','GET'])
+def DoctorRegister():
+    return render_template('DoctorRegister.html')
+
+@Authentication.route("/PatientRegister",  methods=['POST','GET'])
+def PatientRegister():
+    return render_template('PatientRegister.html')
