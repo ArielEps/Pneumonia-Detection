0b152375df358c6841458da39a445847003dddd0
Ariel Eps <butafliko@gmail.com>
2023-05-30 07:57:47 +0300

    start deployment to render

--- PneumoniaApp/__init__.py
+++ PneumoniaApp/__init__.py
@@ -6,9 +6,9 @@ import os
 
 db = SQLAlchemy()
 
-def create_app(database_uri="sqlite:///site.db"):
+def create_app(database_uri="sqlite:///site.db"):     #postgres://biomedical_user:joCzJo5SL3HGYDDZdLudtvjMktcc1CNF@dpg-chqntb64dad3eolf19m0-a.frankfurt-postgres.render.com/biomedical
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_uri
+    app.config["SQLALCHEMY_DATABASE_URI"] =  'postgresql://biomedical_user:joCzJo5SL3HGYDDZdLudtvjMktcc1CNF@dpg-chqntb64dad3eolf19m0-a.frankfurt-postgres.render.com/biomedical'
     
     SECRET_KEY = os.urandom(32)
     app.config['SECRET_KEY'] = SECRET_KEY
