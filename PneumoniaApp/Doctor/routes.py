d81004158d430c77eb5375fb608320af50626bbd
Ariel Eps <butafliko@gmail.com>
2023-06-16 09:55:11 +0300

    create patient history page for doctor

--- PneumoniaApp/Doctor/routes.py
+++ PneumoniaApp/Doctor/routes.py
@@ -21,3 +21,28 @@ def DoctorsAppointments():
     patients = Patient.query.all()
 
     return render_template("DoctorsAppointments.html", appointments = appointment, pat = patients)
+
+@doctor.route("/SearchPatientHistory", methods=['POST', 'GET'])
+@login_required
+def SearchPatientHistory():
+    if request.method == 'POST':
+        patient_id = request.form.get('search')
+
+        patient = Patient.query.filter_by(user_id= patient_id).first()
+        if patient != None:
+            reports = Report.query.filter_by(patient_id=patient.id, is_answer=True)
+            return render_template("DoctorPatientShowHistory.html", reports = reports)
+        else:
+            return render_template("DoctorPatientHistory.html", message = "Not such Patient")
+
+    return render_template("DoctorPatientHistory.html")
+
+@doctor.route("/Doctor_report-details/<int:report_id>")
+@login_required
+def Doctor_report_details(report_id):
+    report = Report.query.get(report_id)
+    if report:
+        doctor = Doctor.query.filter_by(id=report.doctor_id).first()
+        return render_template("DoctorReportDetails.html", report=report, doctor=doctor)
+    else:
+        return render_template("DoctorPatientHistory.html")
